14fcec2a0e262bf01e9cdc0e7c9630da5a76f11b
Yi Ting Wang <y9wang@ucsd.edu>
2021-07-23 16:40:58 +0000

    if-else statement & for loop inside index.html

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     title = "Flask Web App"
-    return render_template("index.html", title=title)
+    dataList = [
+        "Section 1",
+        "Section 2",
+        "Section 3"
+    ]
+    return render_template("index.html", title=title, data=dataList)
 
 if __name__ == "__main__":
     app.run(debug=True)
